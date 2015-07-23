b6158c92de115f5c18c605622cd041c8d5023660
kohrVid <kohrVid@zoho.com>
2015-07-23 15:21:34 +0100

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -14,10 +14,15 @@ class StaticPagesControllerTest < ActionController::TestCase
   end
 
   test "should get about" do
-	  get :about
-	  assert_response :success
-	  assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    get :about
+    assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+  end
 
 end
