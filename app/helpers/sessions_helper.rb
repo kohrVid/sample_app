0b4b8ad518d9476e3503bab09dfa7a57a7ad54d5
jessica <jessica@kandiStryp.kandiStryp>
2015-08-08 23:05:14 +0100

    Added User Update

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -31,11 +31,16 @@ module SessionsHelper
 		@current_user = nil
 	end
 
-	#Forgets a persisitent session
+	#Forgets a persistent session
 	def forget(user)
 		user.forget
 		cookies.delete(:user_id)
 		cookies.delete(:remember_token)
+	end
+
+	#Returns true if the given user is the current user.
+	def current_user?(user)
+		user == current_user
 	end
 
 	#Logs out the current user
@@ -43,6 +48,28 @@ module SessionsHelper
 		forget(current_user)
 		session.delete(:user_id)
 		@current_user = nil
+	end
+
+	#Redirects to stored location (or to the default)
+	def redirect_back_or(default)
+		redirect_to(session[:forwarding_url] || default)
+		session.delete(:forwarding_url)
+	end
+
+	#Stores the URL trying to be accessed.
+	def store_location
+		session[:forwarding_url] = request.url if request.get?
+	end
+
+	#Redirects to stored location (or to the default)
+	def redirect_back_or(default)
+		redirect_to(session[:forwarding_url] || default)
+		session.delete(:forwarding_url)
+	end
+
+	#Stored the URL trying to be accessed.
+	def store_location
+		session[:forwarding_url] = request.url if request.get?
 	end
 
 
