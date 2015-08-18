a6eb82c6addeeffff4e9b6997ef0bf02dc9c0baf
kohrVid <kohrVid@zoho.com>
2015-08-18 19:09:06 +0100

    Add account activations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
-	attr_accessor :remember_token
-	before_save { self.email = email.downcase }
+	attr_accessor :remember_token, :activation_token
+	before_save :downcase_email
+	before_create :create_activation_digest
 
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 	
@@ -42,4 +43,30 @@ class User < ActiveRecord::Base
 	def forget
 		update_attribute(:remember_digest, nil)
 	end
+	
+	#Activates a account
+	def activate
+		update_attribute(:activated, true)
+		update_attribute(:activated_at, Time.zone.now)
+	end
+
+	#Sends activation email
+	def send_activation_email
+		UserMailer.account_activation(self).deliver_now
+	end
+
+
+	private
+
+	#converts email to lower-case
+	def downcase_email
+		self.email = email.downcase
+	end
+
+	#creates and assigns the activation token and digest
+	def create_activation_digest
+		self.activation_token = User.new_token
+		self.activation_digest = User.digest(activation_token)
+	end
+
 end
